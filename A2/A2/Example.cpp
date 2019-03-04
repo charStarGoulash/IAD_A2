b3f7aa010c93cc7088f705e77e49996dcf994b4d
Attila Katona <akatona83@gmail.com>
2019-03-04 18:02:03 -0500

    7

--- A2/A2/Example.cpp
+++ A2/A2/Example.cpp
@@ -215,10 +215,8 @@ int main(int argc, char * argv[])
 				is.read((char*)filePacket, fileLength);
 				firstMessage.theTotalBytes = fileLength;
 				firstMessage.thePacketSize = 256;
-				//int check = CRC::Calculate(filePacket, firstMessage.theTotalBytes, CRC::CRC_32());
+				std::cout << sizeof(filePacket) << std::endl;
 				firstMessage.crc = CRC::Calculate(filePacket, firstMessage.theTotalBytes, CRC::CRC_32());
-
-				std::cout << filePacket << std::endl;
 			}
 
 		}
@@ -386,7 +384,7 @@ int main(int argc, char * argv[])
 				}
 				if (fileDone)
 				{
-					std::cout << packetRec << std::endl;
+					std::cout << sizeof(packetRec) << std::endl;
 					uint32_t crcCheck = CRC::Calculate(packetRec, firstMessage.theTotalBytes, CRC::CRC_32());
 					if (crcCheck == firstMessage.crc)
 					{
